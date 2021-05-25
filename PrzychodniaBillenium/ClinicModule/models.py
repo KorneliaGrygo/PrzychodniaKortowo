f199bcefebbd11cb49e76b5c61733309d82911e7
kpomichowski <kpomichowski@gmail.com>
2021-05-25 23:20:43 +0200

    Additional models - Doctor, Patient, Visits, Visit Category

--- PrzychodniaBillenium/ClinicModule/models.py
+++ PrzychodniaBillenium/ClinicModule/models.py
@@ -15,6 +15,13 @@ class Specialization(models.TextChoices):
     PULMONOLOGIST = 'Pulmonologist', _('Pulmonologist')
 
 
+class VisitCategory(models.TextChoices):
+
+    INITAL = 'Initial visit', _('Initial visit')
+    CONTROL = 'Control visit', _('Control visit')
+    TREATMENT = 'Treatment', _('Symptomatic treatment')
+
+
 class Patient(models.Model):
     """
             Class defines the patient,
@@ -29,7 +36,7 @@ class Patient(models.Model):
 
     address = models.CharField(_('Address'), max_length=64)
 
-    city = models.CharField(_('City'), max_length=64, defualt='Olsztyn')
+    city = models.CharField(_('City'), max_length=64, default='Olsztyn')
 
     zip_code = models.CharField(validators=[validators.validate_zip_code],
                                 help_text=_('Zip code'), max_length=5, default="10117")
@@ -56,4 +63,71 @@ class Doctor(models.Model):
         default=Specialization.INTERNIST
     )
 
-    description = models.CharField(max_length=550)
+    # Order Doctors by ID
+    class Meta:
+        ordering = [
+            'id'
+        ]
+
+    # Description about their proffession, career etc.
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f'Dr. {self.user.first_name} {self.user.second_name}'
+
+
+class Visit(models.Model):
+    """
+    Visit model for patients
+    """
+
+    identificator = models.SlugField(
+        default=validators.get_random_secret,
+        max_length=12,
+        unique=True,
+        editable=False,
+        blank=False
+    )
+
+    date_added = models.DateTimeField(
+        _("Date and time of creation the visit."),
+        auto_now_add=True)
+
+    date_modified = models.DateTimeField(
+        _('Date and time of modification the visit.'),
+        null=True,
+        blank=True,
+        editable=True
+    )
+
+    # Short description of visit
+    description = models.CharField(
+        _('Short description (purpose) of the visit.'),
+        max_length=500,
+        blank=True
+    )
+
+    patient = models.ForeignKey(
+        Patient,
+        on_delete=models.RESTRICT
+    )
+
+    doctor = models.ForeignKey(
+        Doctor,
+        on_delete=models.RESTRICT
+    )
+
+    category = models.CharField(
+        max_length=32,
+        choices=VisitCategory.choices,
+        default=VisitCategory.INITIAL
+    )
+
+    def __str__(self):
+        return f'ID: {self.identificator}'
+
+    class Meta:
+        ordering = [
+            'date_added',
+            'patient'
+        ]
--- PrzychodniaBillenium/ClinicModule/validators.py
+++ PrzychodniaBillenium/ClinicModule/validators.py
@@ -1,4 +1,5 @@
 import re
+import secrets
 from django.db import models
 from django.core.exceptions import ValidationError
 from stdnum.pl import pesel
@@ -19,9 +20,14 @@ def validate_patient_pesel(value):
 # Validator for zip code
 
 def validate_zip_code(value):
-	# Validate the zip code
+        # Validate the zip code
     match = re.search(r'^\d{2}-\d{3}$', value)
     if not match:
         raise ValidationError('The ZIP Code is invalid!')
 
     return value
+
+
+# Get random sequence for Visit ID
+def get_random_secret():
+    return secrets.token_urlsafe(9)
